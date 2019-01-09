82ba9fedfed32b49b237f84b3dd009014c038fc4
1aperez <42749606+1aperez@users.noreply.github.com>
2019-01-09 14:02:27 -0600

    Add files via upload

---
+++ shapeObj.js
@@ -0,0 +1,37 @@
+//Your code
+
+let Rectangle = {
+    length: 4,
+    width: 10,
+
+    perimiter: function(){
+        let p= 2*this.length + 2*this.width;
+        return p;
+    },
+    arena: function(){
+        let a= this.length + 2*this.width;
+        return a;
+    }
+
+}
+
+let Square = {
+   perimiter: function(){
+        let sideLength=36 
+    perimiter }
+    
+
+}
+
+let Circle = {
+
+}
+
+//Dont forget about box and sphere
+
+
+
+
+
+//------------------------------------------------
+//copy paste test code when you finish under here
