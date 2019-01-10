20f5852d8497984dde1f10a1641f415f38ef7b1f
1aperez <42749606+1aperez@users.noreply.github.com>
2019-01-10 13:56:21 -0600

    Add files via upload

--- shapeObj.js
+++ shapeObj.js
@@ -8,7 +8,7 @@ let Rectangle = {
         let p= 2*this.length + 2*this.width;
         return p;
     },
-    arena: function(){
+    area: function(){
         let a= this.length + 2*this.width;
         return a;
     }
@@ -16,20 +16,53 @@ let Rectangle = {
 }
 
 let Square = {
-   perimiter: function(){
+     perimiter: function(){
         let sideLength=36 
-    perimiter }
+     perimiter = sideLength*4
+     function (){
+         area= sideLength*sidelength
+     }
+}
     
 
 }
 
 let Circle = {
+    radius: 15,
+    area: function (){
+        let a =Math.PI* this.radius*this.radius;
+        return a;
+    }
 
 }
 
 //Dont forget about box and sphere
+let Box = {
+    length: 4,
+    width: 10,
+    height: 11,
 
+    volume: function(){
+        let v= this.length* this.height* this.width;
+        return v;
+    },
+    surfaceArea: function(){
+        let sa=  ;
+        return sa;
+    }
+}
 
+let sphere ={
+
+    volume: function(){
+        let v= ;
+        return v;
+    },
+    surfaceArea: function(){
+        let sa= ;
+        return sa
+    }
+}
 
 
 
